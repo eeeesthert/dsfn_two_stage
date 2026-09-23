13f3403cc23b1ec26c363656a24f78ee9d455217
eeeesthert <105769032+eeeesthert@users.noreply.github.com>
2026-09-23 16:51:01 +0800

    Delete UDIS_PyTorch directory

--- UDIS_PyTorch/datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-from .datasets import ABUSPairDataset, AlignmentDataset, ReconstructionDataset, SyntheticHomographyDataset
-
-__all__ = [
-	"ABUSPairDataset",
-	"AlignmentDataset",
-	"ReconstructionDataset",
-	"SyntheticHomographyDataset",
-]
--- UDIS_PyTorch/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import sys
-from pathlib import Path
-
-ROOT = str(Path(__file__).parents[2])
-if ROOT not in sys.path:
-	sys.path.insert(0, ROOT)
